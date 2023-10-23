9e228471e2a6f5c5074d7b31895c5505a038ff92
Eric Han <ejhan@usc.edu>
2023-10-22 21:22:17 -0700

    Basic movement and collisions

---
+++ difficultyproto/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private bool isBlack = true;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetColor();
+    }
+
+    void Update()
+    {
+        // Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, vertical, 0);
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        // Change color
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isBlack = !isBlack;
+            SetColor();
+        }
+    }
+
+    void SetColor()
+    {
+        if (isBlack)
+        {
+            spriteRenderer.color = Color.black;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Black" && !isBlack)
+        {
+            // Handle collision with black bullet when player is white
+            Destroy(gameObject);
+        }
+        else if (other.tag == "White" && isBlack)
+        {
+            // Handle collision with white bullet when player is black
+            Destroy(gameObject);
+        }
+    }
+}
