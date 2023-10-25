9b7242de683d46d758040c2d2b4e6e1cb980f30f
Eric Han <ejhan@usc.edu>
2023-10-24 19:25:29 -0700

    Fixed score

--- difficultyproto/Assets/Scripts/GameManager.cs
+++ difficultyproto/Assets/Scripts/GameManager.cs
@@ -6,30 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
     private int score;
-    private int highScore = 0;
     public TMP_Text scoreText;
     public TMP_Text highScoreText;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            score = 0;
-            highScore = 0;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;  // Exit the Awake method if another instance exists
-        }
+        LoadHighScore();
     }
 
     private void Start()
     {
+        ResetScore();
         UpdateUI();
     }
 
@@ -39,37 +27,55 @@ public class GameManager : MonoBehaviour
         {
             RestartGame();
         }
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            PlayerPrefs.SetInt("HighScore", 0);
+            PlayerPrefs.Save();
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
 #else
-                Application.Quit();
+            Application.Quit();
 #endif
-            highScore = 0;
         }
     }
 
     public void AddScore()
     {
         score++;
-        if (score > highScore)
-        {
-            highScore = score;
-        }
+        SaveHighScore();
         UpdateUI();
     }
 
     private void UpdateUI()
     {
         scoreText.text = "Score: " + score;
-        highScoreText.text = "High Score: " + highScore;
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    private void SaveHighScore()
+    {
+        int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void LoadHighScore()
+    {
+        PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    private void ResetScore()
+    {
+        score = 0;
     }
 
     public void RestartGame()
     {
-        score = 0;  // Reset the current score, but not the high score
+        SaveHighScore();
+        ResetScore();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
